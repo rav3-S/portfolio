1cfacb6cfc34caed0bdc76b3b250b88eb7a18ff7
Ravi <ravishankar.ma34@gmail.com>
2024-10-26 03:24:08 +0530

    added v2 routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import GridBackground from "./components/GridBackground.jsx";
 
-const isV2 = window.location.pathname.includes("v2");
+const Main = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/portfolio" element={<App />} />
+        <Route
+          path="/portfolio/v2"
+          element={
+            <GridBackground>
+              <App />
+            </GridBackground>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {isV2 ? (
-      <GridBackground>
-        <App />
-      </GridBackground>
-    ) : (
-      <App />
-    )}
+    <Main />
   </StrictMode>
 );
