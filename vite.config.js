3a7edddfa66d38e07160cfd2799e437115df9c0f
Ravi <ravishankar.ma34@gmail.com>
2025-07-23 01:24:06 +0530

    Deploying from /docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "",
+  build: {
+    outDir: "docs", // <-- this is what changes the output directory
+  },
+  base: "/",
 });
